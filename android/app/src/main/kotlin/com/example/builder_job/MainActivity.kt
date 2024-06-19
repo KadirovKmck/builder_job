20a246d4c566b170c7b6f42530cc288a84364ec7
KadirovKmck <kadirovmuhammadaziz79@gmail.com>
2024-06-19 14:14:35 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/builder_job/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.builder_job
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'src/presentation/wellcome_view/wellcome_view.dart';
+
+void main(List<String> args) {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: WellcomeView(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
