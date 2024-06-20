01590a73da0b94411d5082cca4100fa473b2975e
KadirovKmck <kadirovmuhammadaziz79@gmail.com>
2024-06-20 16:32:32 +0600

    dddd

--- lib/src/presentation/profile_view/profile_view.dart
+++ lib/src/presentation/profile_view/profile_view.dart
@@ -1,5 +1,5 @@
-import 'package:builder_job/src/presentation/profile_view/widgets/home_view_appbar.dart';
-import 'package:builder_job/src/presentation/profile_view/widgets/home_view_body.dart';
+import 'package:builder_job/src/presentation/profile_view/widgets/profile_view_appbar.dart';
+import 'package:builder_job/src/presentation/profile_view/widgets/profile_view_body.dart';
 import 'package:builder_job/src/utils/media_utils/media_utils.dart';
 import 'package:flutter/material.dart';
 
@@ -12,23 +12,21 @@ class ProfileView extends StatelessWidget {
     final width = MediaUtils.width(context);
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-            height: height,
-            width: width,
-            decoration: const BoxDecoration(
-              color: Color(0xffFFEE1F),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                HomeViewAppBar(width: width, height: height),
-                SizedBox(
-                  height: height * 0.02,
-                ),
-                HomeViewBody(height: height, width: width),
-              ],
-            ),
+        body: Container(
+          height: height,
+          width: width,
+          decoration: const BoxDecoration(
+            color: Color(0xffFFEE1F),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ProfileViewAppBar(width: width, height: height),
+              SizedBox(
+                height: height * 0.04,
+              ),
+              ProfileViewBody(height: height, width: width),
+            ],
           ),
         ),
       ),
